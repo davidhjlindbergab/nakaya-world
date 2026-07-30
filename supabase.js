d7c27788a9ea211eeb81ad9710972e8e68de1b54
davidhjlindbergab <davidhjlindbergab@gmail.com>
2026-07-31 00:54:52 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -10,7 +10,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://fqzfzvlubpfbnimhrgjq.supabase.co";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_ANON_KEY_HERE";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxemZ6dmx1YnBmYm5pbWhyZ2pxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0NDIyNzIsImV4cCI6MjEwMTAxODI3Mn0.j3j7xPv0MmUV1h6vbqEgiNL3gipI4G-lLAhkGI602HI";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: { persistSession: true, autoRefreshToken: true },
